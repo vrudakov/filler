1061291e74626487182b8e8fd9ff078893d17584
vrudakov <rudakov044@gmail.com>
2017-08-06 21:11:45 +0300

    push from clion

--- src/main.c
+++ src/main.c
@@ -13,83 +13,72 @@
 #include <fcntl.h>
 #include "../includes/filler.h"
 #include "../libft/libft.h"
-# define FD 0
+# define FD 3
 
-void    ind_size(t_m *m, char *line)
+void	ind_size(t_m *m, char *line)
 {
-    char    **split;
-    int     x;
-    int     y;
+	char	**split;
+	int		x;
 
-    x = 0;
-    y = 0;
-    split = ft_strsplit(line, ' ');
-    m->size_x = atoi(split[1]);
-    m->size_y = atoi(split[2]);
-    m->map = (char**)malloc(sizeof(char**) * m->size_y);
-    while (x < m->size_x)
-    {
-        m->map[x] = (char*)malloc(sizeof(char*) * m->size_x);
-        x++;
-    }
-    free(split[0]);
-    free(split[1]);
-    free(split[2]);
+	x = 0;
+	split = ft_strsplit(line, ' ');
+	m->size_y = atoi(split[1]);
+	m->size_x = atoi(split[2]);
+	m->map = (char**)malloc(sizeof(char**) * m->size_y);
+	while (x < m->size_x)
+	{
+		m->map[x] = (char*)malloc(sizeof(char*) * m->size_x);
+		x++;
+	}
+	m->map[x] = NULL;
+	free(split[0]);
+	free(split[1]);
+	free(split[2]);
 }
 
-void    fill_map(t_m *m, char *line)
+void	fill_map(t_m *m, char *line)
 {
-    int c;
+	int c;
 
-    c = 0;
-    get_next_line(FD, &line);
-    while (c < m->size_y)
-    {
-        get_next_line(FD, &line);
-        m->map[c] = ft_strdup(line + 4);
-    }
+	c = 0;
+	get_next_line(FD, &line);
+	while (c < m->size_y)
+	{
+		get_next_line(FD, &line);
+		m->map[c] = ft_strdup(line + 4);
+		c++;
+	}
 }
 
-void    to_file(t_m *m, char *str)
+void	to_file(t_m *m, char *str)
 {
-    char *line;
+	char *line;
+	int fd;
 
-    line = "\0";
+	fd = open("in.txt", O_RDONLY);
 
-    FILE *f = fopen("file.txt", "w");
-    while (get_next_line(FD, &line) > 0)
-    {
-        if (m->map == NULL)
-        {
-            if (ft_strstr(line, "$$$"))
-                m->p = line[10] - 48;
-            if (ft_strstr(line, "Plat"))
-                ind_size(m, line);
-        }
-        if (ft_strstr(line, "Plat"))
-            fill_map(m, line);
-        fprintf(f, "%s\n", line);
-    }
-
-
-
-
-
-    if (f == NULL)
-    {
-        printf("Error opening file!\n");
-        exit(1);
-    }
-    fclose(f);
+	line = "\0";
+	while (get_next_line(FD, &line) > 0)
+	{
+		if (m->map == NULL)
+		{
+			if (ft_strstr(line, "$$$"))
+				m->p = line[10] - 48;
+			if (ft_strstr(line, "Plat"))
+				ind_size(m, line);
+		}
+		if (ft_strstr(line, "Plat"))
+			fill_map(m, line);
+	}
 }
 
-int     main(int argc,char **argv)
+int		main(int argc,char **argv)
 {
-    t_m  m;
+	t_m  m;
 
-    m.p = 0;
-    m.map = NULL;
+	m.p = 0;
+	m.map = NULL;
 
-    to_file(&m, argv[1]);
-    return(0);
+	to_file(&m, argv[1]);
+	return(0);
 }
